94d2f3a1f71efd38713c174e4d7e40cfef9f6677
Ghaffaru <mudashiruagm@gmail.com>
2019-11-13 09:55:30 +0000

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'oauth.providers' => \SMartins\PassportMultiauth\Http\Middleware\AddCustomProvider::class,
         'multiauth' => \SMartins\PassportMultiauth\Http\Middleware\MultiAuthenticate::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,7 +6,7 @@ use Route;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
